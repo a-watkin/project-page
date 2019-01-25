3f42cc6b580558656ed5fdd75379002ec25d96ef
adam <atomicpenguines@gmail.com>
2019-01-25 06:42:41 +0000

    some logic to handle missing links

--- projects/projects.py
+++ projects/projects.py
@@ -58,14 +58,15 @@ class Project(object):
         if 'git_link' not in self.__dict__:
             # print('No content, so adding it')
             self.git_link = None
-        # there may not be a git version of some projects
 
+        # there may not be a git version of some projects
         elif self.__dict__['git_link'] is not None and len(self.__dict__['git_link']) == 0:
             self.git_link = None
 
         if 'live_link' not in self.__dict__:
             # print('No content, so adding it')
             self.live_link = None
+
         # there may not be a live version of some projects
         elif self.__dict__['live_link'] is not None and len(self.__dict__['live_link']) == 0:
             self.live_link = None
